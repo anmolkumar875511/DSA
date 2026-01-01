6d49add7311f50c9f1b21def0a235a3bb6a3fa89
Anmol Kumar <anmolkumar875511@gmail.com>
2026-01-01 13:11:21 +0530

    lowerBound (small update)

--- Step4/1DArrayBS/2.cpp
+++ Step4/1DArrayBS/2.cpp
@@ -6,7 +6,7 @@ class Solution{
 public:
     int lowerBound(vector<int> &nums, int x){
 
-        int low = 1;
+        int low = 0;
         int high = nums.size() - 1;
         int ans = nums.size();
         
