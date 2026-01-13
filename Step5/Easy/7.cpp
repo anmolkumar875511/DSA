bdf87cac01d65761fe7385dfbbd37aaa93911a12
Anmol Kumar <anmolkumar875511@gmail.com>
2026-01-13 23:18:41 +0530

    String easy (completed)

--- Step5/Easy/7.cpp
+++ Step5/Easy/7.cpp
@@ -11,10 +11,12 @@ public:
         }
 
         int count[26] = {0};
+        
         for(int i = 0; i < n; i++) {
             count[s[i] - 'a']++;
             count[t[i] - 'a']--;
         }
+
         for(int i = 0; i < 26; i++) {
             if(count[i] != 0) {
                 return false;
