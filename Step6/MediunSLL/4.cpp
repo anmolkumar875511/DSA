ab3afbb2ea34bf41b08d4afced8e0b79fe5e5e65
Anmol Kumar <anmolkumar875511@gmail.com>
2026-01-23 12:54:29 +0530

    hasCycle (added)

---
+++ Step6/MediunSLL/4.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if(fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+
+int main() {
+    Solution sol;
+    ListNode* head = new ListNode(1);
+    ListNode* second = new ListNode(2);
+    ListNode* third = new ListNode(3);
+    head->next = second;
+    second->next = third;
+    third->next = second;
+    cout << sol.hasCycle(head);
+    return 0;
+}
