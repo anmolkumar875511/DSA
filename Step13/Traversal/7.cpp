3d0df14b385f50495cc4847ce9a66fa211383e6a
Anmol Kumar <anmolkumar875511@gmail.com>
2026-03-03 20:52:14 +0530

    postorderTraversal (added)

---
+++ Step13/Traversal/7.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void post_order(vector<int>& v, TreeNode* node) {
+        if(node == nullptr) return ;
+        post_order(v, node->left);
+        post_order(v, node->right);
+        v.push_back(node->val);
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> v;
+        post_order(v, root);
+        return v;
+    }
+};
+
+int main() {
+    /* 
+       Constructing a sample tree:
+             1
+            / \
+           2   3
+          / \
+         4   5
+    */
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    Solution sol;
+    vector<int> results = sol.postorderTraversal(root);
+
+    for (int val : results) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    delete root->left->left;
+    delete root->left->right;
+    delete root->left;
+    delete root->right;
+    delete root;
+}
