5d20cfb59eef537f5d55b794443daf7065b7ef53
Anmol Kumar <anmolkumar875511@gmail.com>
2026-03-05 09:38:09 +0530

    inorderTraversalIterative (added)

---
+++ Step13/Traversal/10.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> inOrder;
+        stack<TreeNode*> st;
+        TreeNode* curr = root;
+        while(true) {
+            if(curr) {
+                st.push(curr);
+                curr = curr->left;
+            }
+            else {
+                if(st.empty()) break;
+                curr = st.top();
+                st.pop();
+                inOrder.push_back(curr->val);
+                curr = curr->right;
+            }
+        }
+        return inOrder;
+    }
+};
+
+int main() {
+    /* 
+       Constructing a sample tree:
+             1
+            / \
+           2   3
+          / \
+         4   5
+    */
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    Solution sol;
+    vector<int> results = sol.inorderTraversal(root);
+
+    for (int val : results) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    delete root->left->left;
+    delete root->left->right;
+    delete root->left;
+    delete root->right;
+    delete root;
+}
