7e32c8f72469b8deedb88b28c6d13521c18c9294
Anmol Kumar <anmolkumar875511@gmail.com>
2026-05-14 22:20:46 +0530

    minValueBST, maxValueBST (added)

---
+++ Step14/Concept/3.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+  public:
+    int minValue(TreeNode* root) {
+        TreeNode* curr = root;
+        while(curr->left) curr = curr->left;
+        return curr->val;
+    }
+    int maxValue(TreeNode* root) {
+        TreeNode* curr = root;
+        while(curr->right) curr = curr->right;
+        return curr->val;
+    }
+};
+
+
+int main() {
+    TreeNode* root = new TreeNode(4);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(7);
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(3);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(9);
+    Solution sol;
+    cout << sol.minValue(root) << endl;
+    cout << sol.maxValue(root) << endl;
+    delete root->left->left;
+    delete root->left->right;
+    delete root->right->left;
+    delete root->right->right;
+    delete root->left;
+    delete root->right;
+    delete root;
+    return 0;
+}
+
