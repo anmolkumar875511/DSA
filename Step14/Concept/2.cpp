63e8466e80cff010db16d7d5b3ac1f87640e6d06
Anmol Kumar <anmolkumar875511@gmail.com>
2026-05-14 22:12:45 +0530

    searchBST (added)

---
+++ Step14/Concept/2.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* searchBST(TreeNode* root, int val) {
+        TreeNode* curr = root;
+        while(curr) {
+            if(curr->val > val) {
+                curr = curr->left;
+            }
+            else if(curr->val < val) {
+                curr = curr->right;
+            }
+            else return curr;
+        }
+        return nullptr;
+    }
+};
+
+int main() {
+    TreeNode* root = new TreeNode(4);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(7);
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(3);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(9);
+    Solution sol;
+    TreeNode* result = sol.searchBST(root, 2);
+    cout << result->val << endl;
+    delete root->left->left;
+    delete root->left->right;
+    delete root->right->left;
+    delete root->right->right;
+    delete root->left;
+    delete root->right;
+    delete root;
+    return 0;
+}
