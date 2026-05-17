813fb63576abeaa62389c29e780cd65c51944c12
Anmol Kumar <anmolkumar875511@gmail.com>
2026-05-17 09:40:05 +0530

    lowestCommonAncestor (added)

---
+++ Step14/Practice/7.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == nullptr) return nullptr ;
+
+        int curr = root->val ;
+
+        if(curr > p->val && curr > q->val){
+            return lowestCommonAncestor(root->left,p,q) ;
+        }
+
+        if(curr < p->val && curr < q->val){
+            return lowestCommonAncestor(root->right,p,q) ;
+        }
+
+        return root ;
+    }
+};
+
+
+int main() {
+    TreeNode* root = new TreeNode(4);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(7);
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(3);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(9);
+    Solution sol;
+    TreeNode* result = sol.lowestCommonAncestor(root, root->left, root->right);
+    cout << result->val << endl;
+    delete root->left->left;
+    delete root->left->right;
+    delete root->right->left;
+    delete root->right->right;
+    delete root->left;
+    delete root->right;
+    delete root;
+    return 0;
+}
