efccc7f8ffbc6a637d69f7b2c37d6d1df80ef797
Anmol Kumar <anmolkumar875511@gmail.com>
2026-05-18 06:25:06 +0530

    findTarget (added)

---
+++ Step14/Practice/11.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    unordered_set<int> st;
+
+    bool findTarget(TreeNode* root, int k) {
+        if(!root) return false;
+
+        if(st.count(k - root->val))
+            return true;
+
+        st.insert(root->val);
+
+        return findTarget(root->left, k) ||
+               findTarget(root->right, k);
+    }
+};
+
+
+int main() {
+    TreeNode* root = new TreeNode(4);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(7);
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(3);
+    root->right->left = new TreeNode(6);
+    root->right->right = new TreeNode(9);
+    Solution sol;
+    cout << sol.findTarget(root, 9) << endl;
+    delete root->left->left;
+    delete root->left->right;
+    delete root->right->left;
+    delete root->right->right;
+    delete root->left;
+    delete root->right;
+    delete root;
+    return 0;
+}
