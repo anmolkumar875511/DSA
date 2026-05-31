3dd9b0f802860ef59cb9e2fcea616f1ebf62cb6f
Anmol Kumar <anmolkumar875511@gmail.com>
2026-05-31 10:13:19 +0530

    postToInfix (added)

---
+++ Step9/OrderConversion/3.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<unordered_map>
+#include<stack>
+using namespace std;
+
+class Solution {
+public:
+    string postToInfix(string &s) {
+        stack<string> st;
+
+        for(char c : s) {
+            if(isalnum(c)) {
+                st.push(string(1, c));
+            }
+            else {
+                string s1 = st.top();
+                st.pop();
+
+                string s2 = st.top();
+                st.pop();
+
+                string expr = "(" + s2 + c + s1 + ")";
+                st.push(expr);
+            }
+        }
+
+        return st.top();
+    }
+};
+
+int main() {
+    Solution sol;
+    string s = "((a-b/c)*(a/k-l))";
+    cout << sol.postToInfix(s);
+    return 0;
+}
