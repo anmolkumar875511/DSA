435314169a6c03b71d325d4d0fb9a322d5f08360
Anmol Kumar <anmolkumar875511@gmail.com>
2026-06-03 06:29:11 +0530

    updateMatrix (added)

---
+++ Step15/Problems_On_BFS&DFS/7.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+using namespace std;
+
+
+class Solution {
+public:
+    vector<int> DIR = {0, 1, 0, -1, 0};
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        queue<pair<int, int>> q;
+        for (int r = 0; r < m; ++r)
+            for (int c = 0; c < n; ++c)
+                if (mat[r][c] == 0) q.emplace(r, c);
+                else mat[r][c] = -1;
+
+        while (!q.empty()) {
+            auto [r, c] = q.front(); q.pop();
+            for (int i = 0; i < 4; ++i) {
+                int nr = r + DIR[i], nc = c + DIR[i+1];
+                if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -1) continue;
+                mat[nr][nc] = mat[r][c] + 1;
+                q.emplace(nr, nc);
+            }
+        }
+        return mat;
+    }
+};
+
+
+int main() {
+    Solution sol;
+    vector<vector<int>> mat = {{0,0,0},{0,1,0},{1,1,1}};
+    vector<vector<int>> result = sol.updateMatrix(mat);
+    for(int i = 0; i < result.size(); i++) {
+        for(int j = 0; j < result[0].size(); j++) {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
