3db16c9cb181f0a0b653f143ec5af5bb5f46e9ef
Anmol Kumar <anmolkumar875511@gmail.com>
2026-06-05 06:57:03 +0530

    findLadders (second approach added)

--- Step15/Problems_On_BFS&DFS/11.cpp
+++ Step15/Problems_On_BFS&DFS/11.cpp
@@ -2,6 +2,9 @@
 #include<vector>
 #include<queue>
 #include<unordered_set>
+#include<unordered_map>
+#include<string>
+#include<functional>
 using namespace std;
 
 class Solution {
@@ -41,6 +44,66 @@ public:
                 s[i] = original;
             }
         }
+        return ans;
+    }
+};
+
+class Solution2 {
+public:
+    vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        vector<vector<string>> ans;
+        if (!dict.count(endWord)) return ans;
+        unordered_map<string, vector<string>> parents;
+        unordered_map<string, int> dist;
+        queue<string> q;
+        q.push(beginWord);
+        dist[beginWord] = 0;
+
+        while (!q.empty()) {
+            string word = q.front();
+            q.pop();
+
+            int level = dist[word];
+            string temp = word;
+            for (int i = 0; i < temp.size(); i++) {
+                char original = temp[i];
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    temp[i] = ch;
+                    if (!dict.count(temp)) continue;
+                    if (!dist.count(temp)) {
+                        dist[temp] = level + 1;
+                        q.push(temp);
+                        parents[temp].push_back(word);
+                    }
+                    else if (dist[temp] == level + 1) {
+                        parents[temp].push_back(word);
+                    }
+                }
+                temp[i] = original;
+            }
+        }
+
+        if (!dist.count(endWord)) return ans;
+        vector<string> path;
+        function<void(string)> dfs = [&](string word) {
+            path.push_back(word);
+
+            if (word == beginWord) {
+                vector<string> curr(path.rbegin(), path.rend());
+                ans.push_back(curr);
+            }
+            else {
+                for (auto &par : parents[word]) {
+                    dfs(par);
+                }
+            }
+
+            path.pop_back();
+        };
+
+        dfs(endWord);
+
         return ans;
     }
 };
