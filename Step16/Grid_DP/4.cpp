bccbca4f066167073c0057f1e258aabda80a76bc
Anmol Kumar <anmolkumar875511@gmail.com>
2026-06-18 09:31:58 +0530

    minPathSum (added)

---
+++ Step16/Grid_DP/4.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<int> dp(n, grid[0][0]);
+        for(int i = 1; i < n; i++) {
+            dp[i] = dp[i - 1] + grid[0][i];
+        }
+        for(int i = 1; i < m; i++) {
+            dp[0] = dp[0] + grid[i][0];
+            for(int j = 1; j < n; j++) {
+                dp[j] = min(dp[j - 1], dp[j]) + grid[i][j];
+            }
+        }
+        return dp[n - 1];
+    }
+};
+
+int main() {
+    Solution s;
+    vector<vector<int>> grid = {{1,3,1},{1,5,1},{4,2,1}};
+    cout << s.minPathSum(grid) << endl;
+    return 0;
+}
