1025747cb7e341765091c2308f5256d6fe2ddcc4
Anmol Kumar <anmolkumar875511@gmail.com>
2026-07-10 09:15:47 +0530

    resetBit (added)

--- Step8/Learning/6.cpp
+++ Step8/Learning/6.cpp
@@ -7,12 +7,18 @@ public:
         n = n | (n + 1);
         return n;
     }
+    int resetBit(int n) {
+        n = n & (n - 1);
+        return n;
+    }
 };
 
 int main() {
     Solution sol;
     int n = 6; // Example input for n
     int result = sol.setBit(n);
-    cout << "Result: " << result << endl;
+    cout << "Set the rightmost unset bit: " << result << endl;
+    result = sol.resetBit(n);
+    cout << "Reset the rightmost set bit: " << result << endl;
     return 0;
 }
