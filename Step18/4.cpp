885b7f88e37e21021f9e89b2ce4d7305e14796fa
Anmol Kumar <anmolkumar875511@gmail.com>
2026-07-21 15:30:26 +0530

    repeatedStringMatch (added)

--- Step18/4.cpp
+++ Step18/4.cpp
@@ -18,16 +18,15 @@ private:
         return {hashValue, pPow};
     }
 
-public:
-    vector<int> rabinKarp(string text, string pattern) {
+    int rabinKarp(const string& text, const string& pattern) {
         int n = text.size();
         int m_len = pattern.size();
         
-        if (m_len > n || m_len == 0) return {};
+        if (m_len > n || m_len == 0) return -1;
 
         long long p = 31;
         long long mod = 1e9 + 7;
-        vector<int> matches;
+        int matches = 0;
 
         auto [hashP, pPow] = findHash(pattern, m_len, p, mod);
         auto [hashT, _] = findHash(text, m_len, p, mod);
@@ -35,15 +34,46 @@ public:
         for (int i = 0; i <= n - m_len; i++) {
             if (hashT == hashP) {
                 if (text.substr(i, m_len) == pattern) {
-                    matches.push_back(i);
+                    matches++;
                 }
             }
 
-            long long leadingChar = (text[i] - 'a' + 1) * pPow % mod;
-            hashT = (hashT - leadingChar + mod) % mod;
-            hashT = (hashT * p + (text[i + m_len] - 'a' + 1)) % mod;
+            if (i < n - m_len) {
+                long long leadingChar = (text[i] - 'a' + 1) * pPow % mod;
+                hashT = (hashT - leadingChar + mod) % mod;
+                hashT = (hashT * p + (text[i + m_len] - 'a' + 1)) % mod;
+            }
         }
 
-        return matches;
+        return matches == 0 ? -1 : matches;
+    }
+
+public:
+    int repeatedStringMatch(string a, string b) {
+        if (a == b) {
+            return 1;
+        }
+        string source = a;
+        int cnt = 1;
+        while (source.size() < b.size()) {
+            source += a;
+            cnt++;
+        }
+        
+        if (rabinKarp(source, b) != -1) {
+            return cnt;
+        }
+        if (rabinKarp(source + a, b) != -1) {
+            return cnt + 1;
+        }
+        return -1;
     }
 };
+
+int main() {
+    Solution sol;
+    string a = "abcd";
+    string b = "cdabcdab";
+    cout << sol.repeatedStringMatch(a, b);
+    return 0;
+}
